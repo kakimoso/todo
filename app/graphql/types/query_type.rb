6e5413fa6801d2f49e2d9c159870c82a4b68855e
kakimoso <k.i.moso9532@gmail.com>
2019-04-09 12:49:26 +0900

    schema吐かせた

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,7 +1,7 @@
 Types::QueryType = GraphQL::ObjectType.define do
   name "Query"
 
-  field :allTasks, !Types::TaskType.to_list_type do
+  field :allTasks, Types::TaskType.to_list_type do
     description "get current_user's tasks"
     resolve ->(obj, args, ctx) {
       ctx[:current_user].tasks.all
