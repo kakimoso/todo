18ffd4ace1c585aae88345ed9cac2c5ad342bc8f
kakimoso <k.i.moso9532@gmail.com>
2019-04-09 11:33:47 +0900

    全タスク取得と検索まで

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -7,4 +7,15 @@ Types::QueryType = GraphQL::ObjectType.define do
       ctx[:current_user].tasks.all
     }
   end
+
+  field :findTaskBy, Types::TaskType.to_list_type do
+    description "get task by id"
+    argument :id, types.Int
+    argument :title, types.String
+
+    resolve ->(obj, args, ctx) {
+      tasks = ctx[:current_user].tasks.where('title like ?', "%#{args.title}%")
+      tasks if tasks
+    }
+  end
 end
